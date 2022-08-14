3126186850297db5ab06d7eb5c529e77c99b0fc0
Mehmet Topuz <mehmettopuz127@gmail.com>
2022-08-15 00:52:04 +0300

    Implemented ping_request function and tested.

--- MQTT-Cryptography/Debug/RingBuffer/ring_buffer.d
+++
@@ -1,4 +0,0 @@
-RingBuffer/ring_buffer.o: ../RingBuffer/ring_buffer.c \
- ../RingBuffer/ring_buffer.h
-
-../RingBuffer/ring_buffer.h:
--- MQTT-Cryptography/TestDebug/ESP8266_Driver/esp8266.d
+++
@@ -1,7 +0,0 @@
-ESP8266_Driver/esp8266.o: ../ESP8266_Driver/esp8266.c \
- ../ESP8266_Driver/esp8266.h \
- C:/Users/Topuz/STM32CubeIDE/workspace_1.6.0/MQTT/MQTT-Cryptography/RingBuffer/ring_buffer.h
-
-../ESP8266_Driver/esp8266.h:
-
-C:/Users/Topuz/STM32CubeIDE/workspace_1.6.0/MQTT/MQTT-Cryptography/RingBuffer/ring_buffer.h:
